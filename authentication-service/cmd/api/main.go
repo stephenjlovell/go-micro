b29f977eb94ce075abb17dabcbcd2b03b3bc521b
Steve Lovell <sjlovell34@gmail.com>
2022-06-02 15:09:15 -0400

    create stub authentication-service

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	models data.Models
+}
+
+func main() {
+	log.Println("starting authentication service...")
+
+	// TODO: connect to DB
+
+	// set up config
+	app := Config{}
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
